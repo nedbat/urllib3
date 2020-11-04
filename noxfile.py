9ffc1506ba72043d8301d071fca4418f43f1b98f
Quentin Pradet <quentin.pradet@gmail.com>
2020-11-04 14:27:20 +0400

    Migrate Python 2.7 -> 3.9/PyPy to GitHub Actions

--- noxfile.py
+++ noxfile.py
@@ -65,7 +65,9 @@ def tests_impl(session, extras="socks,secure,brotli"):
     session.run("coverage", "xml")
 
 
-@nox.session(python=["2.7", "3.5", "3.6", "3.7", "3.8", "3.9", "3.10", "pypy"])
+@nox.session(
+    python=["2.7", "3.5", "3.6", "3.7", "3.8", "3.9", "3.10", "pypy2", "pypy3"]
+)
 def test(session):
     tests_impl(session)
 
